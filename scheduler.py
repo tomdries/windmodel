5a8f65e7a1838663b3d34a1c04506c35a59af075
Tom Driessen <22589103+tomdries@users.noreply.github.com>
2020-04-07 11:11:29 +0200

    added scheduler

---
+++ scheduler.py
@@ -0,0 +1,10 @@
+import schedule
+import time
+from windscraper import *
+
+schedule.every(10).minutes.do(get_zvn_measurement)
+schedule.every(1).hour.do(get_katwijk_measurements)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
