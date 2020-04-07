a85b452a340cfbe4b3f584c940ec960218abf27a
Tom Driessen <22589103+tomdries@users.noreply.github.com>
2020-04-07 11:24:16 +0200

    fix

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,7 @@
 import schedule
 import time
 from windscraper import *
-
+get_zvn_measurement()
 schedule.every(10).minutes.do(get_zvn_measurement)
 schedule.every(1).hour.do(get_katwijk_measurements)
 
